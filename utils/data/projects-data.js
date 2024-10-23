453fabe7a7ba9cc36efb7961cd343c443a4d776a
Peter Ebueku <pebueku@gmail.com>
2024-10-23 02:16:19 +0100

    Updated config

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -8,7 +8,8 @@ export const projectsData = [
     id: 1,
     name: "Field Workforce Application",
     description:
-      "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
+      // "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
+      "The Field Workforce Application (FWA as we often called it), was born from the notion that companies would need an app to be able to manage contractors on the field. We set out to build one app, that was generic and functional enough to be used for a plathora of industries.", // with Capicitor for the mobile app, an ",
     tools: [
       "NestJs",
       "TypeScript",
@@ -33,7 +34,7 @@ export const projectsData = [
     id: 2,
     name: "Kar2Kash",
     description:
-      "I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.",
+      "I built Kar2Kash from the ground up as one of my first real-world projects. Using Redis for queuing financial transactions, Docker Swarm, and CapRover to manage a small fleet of nodes, I worked with real data and real users on an application that handled actual money. With the help of the team, I learned the importance of building technology that met customer needs and added business value, sometimes even at the expense of programming best practices.",
     tools: [
       "NestJs",
       "TypeScript",
@@ -51,41 +52,41 @@ export const projectsData = [
     demo: "",
     image: travel,
   },
-//   {
-//     id: 3,
-//     name: "AI Powered Real Estate",
-//     description:
-//       "My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.",
-//     tools: [
-//       "React",
-//       "Bootstrap",
-//       "SCSS",
-//       "Stripe",
-//       "Express",
-//       "TypeScript",
-//       "MongoDB",
-//       "Azure Blob",
-//       "OpenAI API",
-//       "Replicate AI",
-//       "Cronjob",
-//       "JWT",
-//     ],
-//     code: "",
-//     role: "Full Stack Developer",
-//     demo: "",
-//     image: realEstate,
-//   },
-//   {
-//     id: 4,
-//     name: "Newsroom Management",
-//     description:
-//       "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-//     tools: ["NextJS", "Material UI", "Redux", "Sun Editor", "Calendar"],
-//     code: "",
-//     demo: "",
-//     image: ayla,
-//     role: "Full Stack Developer",
-//   },
+  //   {
+  //     id: 3,
+  //     name: "AI Powered Real Estate",
+  //     description:
+  //       "My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.",
+  //     tools: [
+  //       "React",
+  //       "Bootstrap",
+  //       "SCSS",
+  //       "Stripe",
+  //       "Express",
+  //       "TypeScript",
+  //       "MongoDB",
+  //       "Azure Blob",
+  //       "OpenAI API",
+  //       "Replicate AI",
+  //       "Cronjob",
+  //       "JWT",
+  //     ],
+  //     code: "",
+  //     role: "Full Stack Developer",
+  //     demo: "",
+  //     image: realEstate,
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Newsroom Management",
+  //     description:
+  //       "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+  //     tools: ["NextJS", "Material UI", "Redux", "Sun Editor", "Calendar"],
+  //     code: "",
+  //     demo: "",
+  //     image: ayla,
+  //     role: "Full Stack Developer",
+  //   },
 ];
 
 // Do not remove any property.
